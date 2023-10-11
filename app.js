f91063b28561e72b7c3be6de01e17825c8f58eb7
Johnson1123 <onifadejohnson2017@gmail.com>
2023-10-11 03:42:02 -0700

    Backend work

---
+++ app.js
@@ -0,0 +1,53 @@
+// Day Two
+
+// const app = require("express")();
+// // const path = require("path");
+
+// app.use("/home", (req, res, next) => {
+//   // res.send({ name: "johnson", age: 20, color: "red" });
+//   res.sendFile(__dirname + "/index.html");
+// });
+// app.use("")
+// app.listen(3000, () => {
+//   console.log("Server running successful");
+// });
+
+// ------------ Day 3 -------------------
+
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const bodyPaser = require("body-parser");
+
+app.use(bodyPaser.urlencoded({ extended: true }));
+
+// app.use("/product", (req, res, next) => {
+//   // res.send({ name: "johnson", age: 20, color: "red" });
+//   req.body = {};
+//   next();
+// });
+
+// app.use("/", (req, res) => {
+//   // res.sendFile(__dirname + "/index.html");
+//   res.send(req.body);
+// });
+
+app.use("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "register.html"));
+});
+
+app.use("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "login.html"));
+});
+
+app.use("/app/register", (req, res) => {
+  res.redirect("/login");
+});
+app.use("/home", (req, res) => {
+  res.send(req.body);
+});
+
+app.listen(3000, () => {
+  console.log("Server running successful");
+});
